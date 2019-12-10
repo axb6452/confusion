8903af95c7c23bac513220b55c6885a28096fe16
axb6452 <ashok.balakumar@gmail.com>
2019-12-09 21:28:37 -0500

    Combining reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,24 @@
 // Configure store in this file and export 
-import { createStore } from 'redux'; //allows creation of redux store
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux'; //allows creation of redux store; combineReducers enables individual reducers to be combined.
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        // Reducer,
+        // initialState
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
 
     return store;
